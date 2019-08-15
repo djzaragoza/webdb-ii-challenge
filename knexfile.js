f572431025c2713019d41d2b175a9bf0d26de6c6
unknown <dj.zaragoza@icloud.com>
2019-08-15 16:28:10 -0700

    configuring knex file

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/database_file.db3',
+    },
+    //necessary when using sqlite3
+    useNullAsDefault: true
   }
 
 };
